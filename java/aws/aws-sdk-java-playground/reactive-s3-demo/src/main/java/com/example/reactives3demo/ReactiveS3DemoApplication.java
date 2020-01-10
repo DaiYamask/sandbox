bb5173bb2747f4b52ee94ffb282c94f12e9a1573
dai <dai@tagbangers.co.jp>
2020-01-10 14:52:58 +0900

    reactive s3 demo

---
+++ java/aws/aws-sdk-java-playground/reactive-s3-demo/src/main/java/com/example/reactives3demo/ReactiveS3DemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.reactives3demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactiveS3DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactiveS3DemoApplication.class, args);
+	}
+
+}
